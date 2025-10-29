c157460f0b6c524587f3d9cb57a5e74a1823688a
faiza fehri <faiza.fehri+github@gmail.com>
2025-10-29 06:05:23 -0400

    upload fallback

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center bg-paperbg">
+      <p className="text-3xl font-sourcecode">Chargement du contenu</p>
+    </div>
+  );
+}
